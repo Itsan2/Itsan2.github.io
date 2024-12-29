d8daa243a8dded279211a8a90955903cc1f95981
Itsan <166802033+Itsan2@users.noreply.github.com>
2024-12-29 17:31:10 +0800

    Add files via upload

--- mainweb/script.js
+++ mainweb/script.js
@@ -1,3 +1,25 @@
+// Smooth Scroll Functionality
+document.querySelectorAll('.nav-links a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        const targetId = this.getAttribute('href'); // Get the target element's ID
+
+        // Cek apakah targetId adalah ID elemen di halaman yang sama
+        if (targetId.startsWith('#')) {
+            e.preventDefault(); // Prevent default anchor click behavior
+
+            const targetElement = document.querySelector(targetId); // Select the target element
+
+            // Scroll to the target element
+            targetElement.scrollIntoView({
+                behavior: 'smooth', // Smooth scroll
+                block: 'start' // Align to the start of the target element
+            });
+        }
+        // Jika bukan ID, biarkan tautan berfungsi normal (misalnya, tautan ke halaman lain)
+    });
+});
+
+
 document.getElementById('dev').addEventListener('click', function(event) {
     event.preventDefault(); // Mencegah link default
     alert('Active Developer');
@@ -43,6 +65,5 @@ document.getElementById('steamButton').addEventListener('click', function() {
 document.getElementById('githubButton').addEventListener('click', function() {
     alert('Go to Github.');
 });
-
 
 
